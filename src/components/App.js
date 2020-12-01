39d8787ce0f560650d028e9b2e672bd45d8f8534
Linguistics <thylinguistics@gmail.com>
2020-11-30 22:50:35 -0800

    ...

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,11 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
 import Mecury from './planets/Mecury';
+import Venus from './planets/Venus';
 import Mars from './planets/Mars';
 import LandsatEarth from './LandsatEarth';
+import PlanetList from './PlanetList';
+import Footer from './Footer';
 import '../styles/App.css';
 
 
@@ -13,12 +16,15 @@ const App = ()=>{
     return(
         <Router>
             <Header />
-            <div className="container m-auto my-12">
+            <div className="container m-auto">
             <Route path="/" exact component={Home} />
             <Route path="/planet/mecury" exact component={Mecury} />
+            <Route path="/planet/venus" exact component={Venus} />
             <Route path="/planet/mars" exact component={Mars} />
             <Route path="/landsat/earth" exact component={LandsatEarth} />
+            <Route path="/planets" exact component={PlanetList} />
             </div>
+            <Footer />
         </Router>
     )
 };
--- src/components/PlanetList.js
+++ src/components/PlanetList.js
@@ -3,14 +3,13 @@ import { Link } from 'react-router-dom';
 
 const PlanetList = () => {
     return (
-        <div>
-            <h2>Learn more about the solar system:</h2>
-           <Link to="/planet/earth"><li>Earth</li></Link>
-           <Link to="/planet/mars"><li>Mars</li></Link>
-           <Link to="/planet/saturn"><li>Saturn</li></Link>
-           <div>
-               <p>sjfljdkljdsk</p>
-           </div>
+        <div className="text-center h-screen my-32">
+            <h2 className="text-2xl mb-5">Learn more about the solar system:</h2>
+           <Link className="text-lg" to="/planet/mecury"><li>Mecury</li></Link>
+           <Link className="text-lg" to="/planet/venus"><li>Venus</li></Link>
+           <Link className="text-lg" to="/planet/earth"><li>Earth</li></Link>
+           <Link className="text-lg" to="/planet/mars"><li>Mars</li></Link>
+           <Link className="text-lg" to="/planet/saturn"><li>Saturn</li></Link>
         </div>
     )
 }
