129ee3c34361e591ef195b221ba86aeafb00bf20
Linguistics <thylinguistics@gmail.com>
2020-12-04 21:54:56 -0800

    keeping KEY on host

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import PlanetList from './PlanetList';
-import axios from 'axios';
+//import axios from 'axios';
 import { KEY } from  '../nasaKey';
 
 
