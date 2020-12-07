06b246c79d27725d273852b21c5f0eb21f79d222
Linguistics <thylinguistics@gmail.com>
2020-12-07 12:40:53 -0800

    added menu toggle to links/for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -46,9 +46,9 @@ const Header = () => {
     
       <div className={menu === true? 'block sm:block': 'hidden sm:hidden'}>
         <div className="px-2 pt-2 pb-3">
-          <Link to="/planets" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Planets</Link>
-          <Link to="/landsat/earth" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">LandSat</Link>
-          <Link to="/planet/mars" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Mars Rover</Link>
+          <Link onClick={()=>menuButton()} to="/planets" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Planets</Link>
+          <Link onClick={()=>menuButton()} to="/landsat/earth" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">LandSat</Link>
+          <Link onClick={()=>menuButton()} to="/planet/mars" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Mars Rover</Link>
         </div>
       </div>
     </nav>
